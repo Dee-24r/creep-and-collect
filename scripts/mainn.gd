d17945ceabc6dbd7a97f55a8a944ff0c75c67c55
Funmilayo <fummyoshe@gmail.com>
2026-05-01 22:46:16 -0400

    Added exit

--- scripts/mainn.gd
+++ scripts/mainn.gd
@@ -1,5 +1,6 @@
 extends Node2D
 var score: int = 0
+var level: int = 1
 @onready var score_label: Label = $display/scorePanel/score_Label
 @onready var winning_sound: AudioStreamPlayer2D = $LevelRoot/player/winningSound
 signal win
@@ -13,6 +14,11 @@ func _process(_delta: float) -> void:
 	pass
 
 func _setup_level() -> void:
+	#exit
+	var exit = $LevelRoot.get_node_or_null("Exit")
+	if exit:
+		exit.body_entered.connect(_on_exit_body_entered)
+	
 	#bananas
 	var bananas = $LevelRoot.get_node_or_null("bananas")
 	if bananas:
@@ -25,7 +31,15 @@ func _setup_level() -> void:
 		for enemy in enemies.get_children():
 			enemy.player_failed.connect(_on_player_failed)
 
+
 #signals
+
+func _on_exit_body_entered(body: Node2D)-> void:
+	if body.name == "player":
+		level+=1
+		print(level)
+		body.can_move = false
+
 func _on_player_failed(body):
 	body.fail()
 	print("Player failed")
--- scripts/player.gd
+++ scripts/player.gd
@@ -6,6 +6,7 @@ extends CharacterBody2D
 const SPEED = 300.0
 const JUMP_VELOCITY = -850.0
 var alive = true
+var can_move = true
 
 func _physics_process(delta: float) -> void:
 	#Add animation
@@ -22,24 +23,25 @@ func _physics_process(delta: float) -> void:
 		velocity += get_gravity() * delta
 		animated_sprite_2d.animation = "jumping"
 	
-	# Handle jump.
-	if Input.is_action_just_pressed("jump") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-		jumpsound.play()
+	if can_move:
+		# Handle jump.
+		if Input.is_action_just_pressed("jump") and is_on_floor():
+			velocity.y = JUMP_VELOCITY
+			jumpsound.play()
 
-	# Get the input direction and handle the movement/deceleration.
-	var direction := Input.get_axis("left", "right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		# Get the input direction and handle the movement/deceleration.
+		var direction := Input.get_axis("left", "right")
+		if direction:
+			velocity.x = direction * SPEED
+		else:
+			velocity.x = move_toward(velocity.x, 0, SPEED)
 
-	move_and_slide()
-	
-	if direction == 1.0:
-		animated_sprite_2d.flip_h = false
-	elif direction == -1.0:
-		animated_sprite_2d.flip_h = true
+		move_and_slide()
+		
+		if direction == 1.0:
+			animated_sprite_2d.flip_h = false
+		elif direction == -1.0:
+			animated_sprite_2d.flip_h = true
 
 func fail() -> void:
 	failed_sound.play()
