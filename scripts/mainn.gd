4dd4a9b266ca30669caedd9cae3d0b3060b6a03a
Funmilayo <fummyoshe@gmail.com>
2026-05-08 01:46:37 -0400

    Level 4 and really spec music

--- scripts/mainn.gd
+++ scripts/mainn.gd
@@ -1,8 +1,9 @@
 extends Node2D
-var LASTLEVEL = 3
+var LASTLEVEL = 4
 var score: int = 0
-var level: int = 1
+var level: int = 4
 var current_level_root:Node = null
+@onready var message: RichTextLabel = $levelMessage
 
 @onready var fade: ColorRect = $display/Fade
 @onready var score_label: Label = $display/scorePanel/score_Label
@@ -42,6 +43,7 @@ func _setup_level(level_root: Node) -> void:
 	var exit = level_root.get_node_or_null("Exit")
 	if exit:
 		exit.body_entered.connect(_on_exit_body_entered)
+
 	
 	#bananas
 	var bananas = level_root.get_node_or_null("bananas")
@@ -56,6 +58,15 @@ func _setup_level(level_root: Node) -> void:
 			enemy.player_failed.connect(_on_player_failed)
 
 
+#LEVEL MESSAGE
+func show_message(text):
+	message.text = text
+	message.visible = true
+	
+	await get_tree().create_timer(2.0).timeout
+	message.visible = false
+	
+		
 #signals
 func _on_exit_body_entered(body: Node2D)-> void:
 	if body.name == "player":
@@ -70,6 +81,7 @@ func _on_exit_body_entered(body: Node2D)-> void:
 
 func _on_player_failed(body):
 	body.fail()
+	level = 1
 	await _load_level(level, false, true)
 
 func _winner() -> void:
@@ -81,7 +93,7 @@ func _winner() -> void:
 func _increase_score() -> void:
 	score += 5
 	score_label.text = "SCORE: %s" % score
-	if score == 20 || score == 50 || score == 85:
+	if score == 20 || score == 50 || score == 100:
 		_winner()
 		
 
